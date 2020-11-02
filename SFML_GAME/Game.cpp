65cb8e1db196f97a187eb60586991e22fb81204a
PHANCHITA TERAPHATANAROJ <63010665@KMITL.AC.TH>
2020-11-03 02:30:07 +0700

    END CLIP 12 MOUSE POSITIONS AND ENGINE STUFF

--- SFML_GAME/Game.cpp
+++ SFML_GAME/Game.cpp
@@ -59,7 +59,7 @@ void Game::initKeys()
 
 void Game::initStates()
 {
-	this->states.push(new GameState(this->window,&this->supportedKeys));
+	this->states.push(new MainMenuState(this->window, &this->supportedKeys));
 }
 //Constructors/Destructors
 Game::Game()
--- SFML_GAME/GameState.cpp
+++ SFML_GAME/GameState.cpp
@@ -2,11 +2,18 @@
 
 void GameState::initKeybinds()
 {
-	this->keybinds["CLOSE"] = this->supportedKeys->at("Escape");
-	this->keybinds["MOVE_LEFT"] = this->supportedKeys->at("A");
-	this->keybinds["MOVE_RIGHT"] = this->supportedKeys->at("D");
-	this->keybinds["MOVE_UP"] = this->supportedKeys->at("W");
-	this->keybinds["MOVE_DOWN"] = this->supportedKeys->at("S");
+	std::ifstream ifs("Config/gamestate_keybinds.ini");
+	if (ifs.is_open())
+	{
+		std::string key = "";
+		std::string key2 = "";
+		while (ifs >> key >> key2)
+		{
+			this->keybinds[key] = this->supportedKeys->at(key2);
+		}
+	}
+	ifs.close();
+
 }
 
 GameState::GameState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys)
@@ -14,6 +21,7 @@ GameState::GameState(sf::RenderWindow* window, std::map<std::string, int>* suppo
 {
 	this->initKeybinds();
 }
+
 GameState::~GameState()
 {
 
@@ -41,6 +49,7 @@ void GameState::updateInput(const float& dt)
 
 void GameState::update(const float& dt)
 {
+	this->updateMousePosition();
 	this->updateInput(dt);
 
 	this->player.update(dt);
