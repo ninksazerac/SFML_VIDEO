2bfe958e5bc57a697a847f900f7ecc01fd128e8a
PHANCHITA TERAPHATANAROJ <63010665@KMITL.AC.TH>
2020-11-04 00:05:11 +0700

    start clip 16

--- SFML_GAME/Button.h
+++ SFML_GAME/Button.h
@@ -12,9 +12,13 @@
 #include"SFML\Graphics.hpp"
 #include"SFML\Audio.hpp"
 
+enum button_states { BTN_IDLE = 0, BTN_HOVER, BTN_ACTIVE };
+
 class Button
 {
 private:
+	short unsigned buttonState;
+
 	sf::RectangleShape shape;
 	sf::Font* font;
 	sf::Text text;
@@ -30,6 +34,9 @@ public:
 		sf::Font* font, std::string text, 
 		sf::Color idleColor, sf::Color hoverColor, sf::Color activeColor);
 	~Button();
+
+	//Accessors
+	const bool isPressed() const;
 
 	//Functions
 	void update(const sf::Vector2f mousePos);
--- SFML_GAME/MainMenuState.cpp
+++ SFML_GAME/MainMenuState.cpp
@@ -24,18 +24,37 @@ void MainMenuState::initKeybinds()
 
 }
 
+void MainMenuState::initButtons()
+{
+	this->buttons["GAME_STATE"] = new Button(100, 100, 150, 50,
+		&this->font, "New Gamer",
+		sf::Color(70, 70, 70, 200), sf::Color(150, 150, 150, 200), sf::Color(20, 20, 20, 200));
+
+	this->buttons["EXIT_STATE"] = new Button(100, 300, 150, 50,
+		&this->font, "Quit",
+		sf::Color(100, 100, 100, 200), sf::Color(150, 150, 150, 200), sf::Color(20, 20, 20, 200));
+}
+
 MainMenuState::MainMenuState(sf::RenderWindow* window, std::map<std::string, int>* supportedKeys)
 	: State(window, supportedKeys)
 {
 	this->initFonts();
 	this->initKeybinds();
+	this->initButtons();
+
+	
 
 	this->background.setSize(sf::Vector2f(window->getSize().x, window->getSize().y));
 	this->background.setFillColor(sf::Color::Magenta);
 }
+
 MainMenuState::~MainMenuState()
 {
-
+	auto it = this->buttons.begin();
+	for (it = this->buttons.begin(); it != this->buttons.end(); ++it)
+	{
+		delete it->second;
+	}
 }
 
 void MainMenuState::endState()
@@ -51,13 +70,44 @@ void MainMenuState::updateInput(const float& dt)
 	
 }
 
+void MainMenuState::updateButtons()
+{
+	/*Update all the button in the state and handles their functionlaity.*/
+	for (auto &it : this->buttons)
+	{
+		it.second->update(this->mousePosView);
+	}
+
+	//New game
+	if (this->buttons["GAME_STATE"]->isPressed())
+	{
+		//this->states.push(new MainMenuState(this->window, &this->supportedKeys));
+	}
+
+	//Quit the game
+	if (this->buttons["EXIT_STATE"]->isPressed())
+	{
+		this->quit = true;
+	}
+}
+
 void MainMenuState::update(const float& dt)
 {
 	this->updateMousePosition();
 	this->updateInput(dt);
 
-	
+	this->updateButtons();
+
 }
+
+void MainMenuState::renderButtons(sf::RenderTarget* target)
+{
+	for (auto& it : this->buttons)
+	{
+		it.second->render(target);
+	}
+}
+
 
 void MainMenuState::render(sf::RenderTarget* target)
 {
@@ -65,6 +115,6 @@ void MainMenuState::render(sf::RenderTarget* target)
 		target = this->window;
 	target->draw(this->background);
 
-
+	this->renderButtons(target);
 
 }
