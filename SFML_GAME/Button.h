5b680aef2020daa74ceb3096c319a4549c06c4fc
PHANCHITA TERAPHATANAROJ <63010665@KMITL.AC.TH>
2020-11-03 10:56:19 +0700

    END CLIP 13 START CLIP 14

--- SFML_GAME/Button.h
+++ SFML_GAME/Button.h
@@ -1,13 +1,39 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include <sstream>
+
+
+#include"SFML\System.hpp"
+#include"SFML\Window.hpp"
+#include"SFML\Graphics.hpp"
+#include"SFML\Audio.hpp"
 
 class Button
 {
 private:
+	sf::RectangleShape shape;
+	sf::Font* font;
+	sf::Text text;
+
+	sf::Color idleColor;
+	sf::Color hoverColor;
+	sf::Color activeColor;
+
+
+
 public:
-	Button();
+	Button(float x, float y, float width, float height, 
+		sf::Font* font, std::string text, 
+		sf::Color idleColor, sf::Color hoverColor, sf::Color activeColor);
 	~Button();
+
+	//Functions
+	void update(const sf::Vector2f mousePos);
+	void render(sf::RenderTarget* target);
 
 };
 #endif // !BUTTON_H
